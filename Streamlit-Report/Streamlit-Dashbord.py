75fc97513a47c0378507aec1d9309aaa94b367d0
Your Name <you@example.com>
2022-08-30 20:15:17 -0300

    updated

--- Streamlit-Report/Streamlit-Dashbord.py
+++ Streamlit-Report/Streamlit-Dashbord.py
@@ -238,7 +238,7 @@ def business_report(data, geofile):
 if __name__ == '__main__':
     # ETL
     # Data Extraction
-    path = "Streamlit-Dashbord.py"
+    path = "kc_house_data.csv"
     url = 'https://opendata.arcgis.com/datasets/83fc2e72903343aabff6de8cb445b81c_2.geojson'
 
     data = get_data(path)
