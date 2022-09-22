2b7881a4f6014b9e35c0ed4be367aee1776bb85f
Your Name <you@example.com>
2022-09-22 15:43:09 -0300

    updated deploy

--- Streamlit-Report/Streamlit-Dashbord.py
+++ Streamlit-Report/Streamlit-Dashbord.py
@@ -7,6 +7,9 @@ import streamlit as st
 import plotly.express as px
 import folium 
 import geopandas
+import streamlit as st
+from google.oauth2 import service_account
+from google.cloud import storage
 
 
 
@@ -24,9 +27,71 @@ def get_data(path):
     return data
 
 
+
 @st.cache(allow_output_mutation=True)
 def convert_data(data):
     return data.to_csv()
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(st.secrets["gcp_service_account"])
+
+client = storage.Client(credentials= credentials)
+
+@st.experimental_memo(ttl=600)
+def read_file(bucket_name, file_path):
+    bucket = client.bucket(bucket_name)
+    content = bucket.blob(file_path).download_as_string().decode("utf-8")
+    return content
+
+
+# functions
+def get_data(path):
+    data = pd.read_csv(path)
+
+
+    return data
+
+def transform(data):
+    # Data types
+    data['date'] = pd.to_datetime(data['date'])
+    # Cleaning
+    data.loc[data['bedrooms']==33,'bedrooms'] = data['bedrooms'].median()
+    data = data[(data['bedrooms']!=0) | (data['bathrooms'] != 0)]
+    data = data.sort_values('date', ascending=False)
+    data = data.drop_duplicates('id')
+
+    # Set features
+    data['month-day'] = data['date'].dt.strftime('%m-%d')
+    data['season_year'] = data['month-day'].apply(lambda x: 'summer' if x >= '06-21' and x < '09-22' else     
+                                                      'fall' if x >= '09-22' and x < '12-21' else 
+                                                      'spring' if x >= '03-20' and x < '06-20' else
+                                                      'winter')
+    price_reg_grouped = data[['price','zipcode']].groupby('zipcode').median().reset_index()
+    m1 = data.merge(price_reg_grouped, on=['zipcode'], validate='many_to_one', how='inner')                                       
+    price_regssn_grouped = m1[['price_x','season_year', 'zipcode']].groupby(['zipcode','season_year']).median().reset_index()
+    m2 = m1.merge(price_regssn_grouped, on=['zipcode','season_year'],validate= 'many_to_one', how='inner')
+    m2.columns = ['id', 'date', 'price', 'bedrooms', 'bathrooms', 'sqft_living',
+       'sqft_lot', 'floors', 'waterfront', 'view', 'condition', 'grade',
+       'sqft_above', 'sqft_basement', 'yr_built', 'yr_renovated', 'zipcode',
+       'lat', 'long', 'sqft_living15', 'sqft_lot15', 'month-day',
+       'season_year', 'median_price', 'median_s_price']
+    data = m2.copy()
+    data['status'] = data.apply(lambda x: 'buy' if x['price'] < x['median_price'] and x['condition'] >= 3 else 'do not buy' ,axis = 1)
+    data['sale_price'] = data.apply(lambda x: 1.3*x['price'] if x['price'] > x['median_s_price'] else 1.1*x['price'], axis=1)
+    data['profit'] = data.apply(lambda x: x['sale_price'] - x['price'], axis=1)
+
+    data['sqft_diff15_num'] = data.apply(lambda x: x['sqft_living'] - x['sqft_living15'], axis = 1)
+    data['renovation_space'] = data.apply(lambda x: x['sqft_lot']-x['sqft_living'], axis=1)
+    data['renovation'] = data.apply(lambda x: 'yes' if (x['sqft_diff15_num'] < 0)  & ((x['sqft_lot']-x['sqft_living']) > 0) else 'no', axis= 1 )
+    data['renovation_area'] = data.apply(lambda x: x['sqft_diff15_num']*(-1) if (x['renovation'] == 'yes') and (x['sqft_diff15_num']*(-1) <= 0.25*x['sqft_living']) else
+                                             0.25*x['sqft_living'] if (x['renovation'] == 'yes') and (x['sqft_diff15_num']*(-1) > 0.25*x['sqft_living']) else
+                                             0, axis=1 )
+    data['sqft_price'] = data.apply(lambda x: x['price']/x['sqft_living'], axis=1)
+    data['price_increasing'] = data.apply(lambda x: x['sqft_price']*x['renovation_area'], axis=1)
+    data['sale_price_renovated'] = data.apply(lambda x: x['sale_price'] + x['price_increasing'], axis=1)
+    data['profit_renovated'] = data.apply(lambda x: x['sale_price_renovated']-x['price'], axis=1)
+
+    return data
     
 
 def filters(data):
@@ -145,6 +210,8 @@ def business_report(data):
     
     with tab1:
         folium_static(density_map)
+    
+    return data
 # ==============================================================================================================
 # TAB 2
 # ==============================================================================================================
@@ -192,17 +259,22 @@ def business_report(data):
 if __name__ == '__main__':
     # ETL
     # Data Extraction
-    path = "data_transformed.csv"
+    path = "gs://house-apps-sales/kc_house_data.csv"
     url = 'https://opendata.arcgis.com/datasets/83fc2e72903343aabff6de8cb445b81c_2.geojson'
 
-    data = get_data(path)
+    bucket_name = "house-apps-sales"
+    file_path = "kc_house_data.csv"
+    
+    data = read_file(bucket_name, file_path)
 
     # Transformation
+
+    data = transform(data)
     
     f_price, f_zipcode, f_bedrooms, f_bathrooms, f_waterview = filters(data)
 
     # Load
-    overview(data)
+    data = overview(data)
     business_report(data)
 
 
